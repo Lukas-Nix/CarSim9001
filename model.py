7d0aa4d7fbf1efafdbd159df3ddd19f1ddb02d36
Nix <35922475+Lukas-Nix@users.noreply.github.com>
2019-09-05 13:20:10 +0200

    glemt det meste, men prøver mig frem

--- model.py
+++ model.py
@@ -4,13 +4,40 @@ class Car(object):
     pass
 
 class Wheel(object):
-    pass
+    def __init__(self):
+        self.orientation = randint(0,360)
 
 class Engine(object):
-    pass
+        def __init__(self):
+            self.self.throttlePosition=0
+            self.self.theGearbox=Gearbox()
+            self.currentRpm=0
+            self.consumptionConstant=0.0025
+            self.maxRpm=100
+            self.theTank=Tank()
+
+        def updateModel(self, dt):
+            if theTank > 0:
+                self.currentRpm=self.throttlePosition*self.maxRpm
 
 class Gearbox(object):
-    pass
+    def __init__(self):
+        self.gears = [0,0.8,1,1.4,2.2,3.8]
+        self.currentGear = 0
+        self.clutchEngaged = False
+        self.wheels = ['frontLeft', 'frontRight', 'rearLeft', 'rearRight']
+
+    def shiftUp(self):
+        if self.currentGear < len(self.gears) or self.clutchEngaged = True:
+            self.currentGear = self.currentGear +1
+
+    def shiftDown(self):
+        if self.currentGear > 0 or self.clutchEngaged = True:
+            self.currentGear = self.currentGear -1
+
+    #def rotate(self, revolutions):
+    #    if self.clutchEngaged = True:
+    #        rotate(Wheel())
 
 class Tank(object):
     pass
